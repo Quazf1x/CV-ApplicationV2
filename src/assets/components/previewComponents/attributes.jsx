9c8c88069a0dba8354e070e0c40ca00eaf86169c
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-07-27 16:22:14 +0600

    Ver 0.2.13

--- src/assets/components/previewComponents/attributes.jsx
+++ src/assets/components/previewComponents/attributes.jsx
@@ -2,24 +2,53 @@
 
 function Attribute({isOutlined = false, name, dataKey, infoState, type = 'text'}) {
 
-  if(type === 'text') 
-  return(
-    <div className={isOutlined ? 'top-border' + ' attribute-list': 'attribute-list'}>
-      <h2 className="attribute-name">{name}</h2>
-      <div className='text-area-div' >
-        {infoState[dataKey]}
+  const content = type === 'text' ? 
+    <div className='text-area-div'>
+      {infoState[dataKey]}
+    </div> 
+    : 
+    <div>
+      <div>
+        <h2>Computer Science</h2>
+        <p>
+          <span>Sep 1989</span> — <span>Sep 1992</span>
+        </p>
       </div>
-    </div>
-  );
-  else return(
-    <div className={isOutlined ? 'top-border' + ' attribute-list' : 'attribute-list'}>      
-      <h2 className="attribute-name">{name}</h2>
-      <ul className="attribute-ul-list">
-        <li>11</li>
-        <li>22</li>
-      </ul>
-    </div>
-  )
+      <p>
+        University of Washington
+      </p>
+      <p>
+        United States
+      </p>
+    </div>;
+
+  return(
+      <div className={isOutlined ? 'top-border' + ' attribute-list': 'attribute-list'}>
+        <h2 className="attribute-name">{name}</h2>
+        {content}
+      </div>
+    );
+
+
+  // if(type === 'text') 
+  // return(
+  //   <div className={isOutlined ? 'top-border' + ' attribute-list': 'attribute-list'}>
+  //     <h2 className="attribute-name">{name}</h2>
+  //     <div className='text-area-div' >
+  //       {infoState[dataKey]}
+  //     </div>
+  //   </div>
+  // );
+  // else return(
+  //   <div className={isOutlined ? 'top-border' + ' attribute-list' : 'attribute-list'}>      
+  //     <h2 className="attribute-name">{name}</h2>
+  //     <ul className="attribute-ul-list">
+  //       <li>11</li>
+  //       <li>22</li>
+  //     </ul>
+  //   </div>
+  // )
+
 }
 
 export default Attribute;
