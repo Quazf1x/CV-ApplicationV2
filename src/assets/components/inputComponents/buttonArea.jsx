29e94a5ad46a8dbc2dedd6c5151323e16ec2e310
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2023-08-07 18:50:01 +0600

    Ver 0.5.1

--- src/assets/components/inputComponents/buttonArea.jsx
+++ src/assets/components/inputComponents/buttonArea.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 function ButtonArea({fillResume, clearResume}) {
+
+  const downloadPDF = () => {
+    console.log('do later'); 
+  }
+
   return(
     <div className="button-section input-section-padding pretty-background">
       <button onClick={fillResume} className="button-template">Load Template</button>
-      <button className="button-template">
+      <button onClick={downloadPDF} className="button-template">
       <i className="fa-solid fa-file-arrow-down"></i>
         Download PDF
       </button>
